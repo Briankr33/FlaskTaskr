30e0511eec3980f0667e66c1313a1471bcfee22a
Your Name <you@example.com>
2019-01-09 10:10:27 -0600

    Add user registration

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from functools import wraps
-from flask import Flask, flash, redirect, render_template, request, session, url_for, g
-from forms import AddTaskForm
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from forms import AddTaskForm, RegisterForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 
 # config
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config.from_object('_config')
 db = SQLAlchemy(app)
 
-from models import Task
+from models import Task, User
 
 # helper functions
 def login_required(test):
@@ -79,3 +79,20 @@ def delete_entry(task_id):
 	db.session.commit()
 	flash("The task was deleted.")
 	return redirect(url_for('tasks'))
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+	error = None
+	form = RegisterForm(request.form)
+	if request.method == 'POST':
+		if form.validate_on_submit():
+			new_user = User(
+				form.name.data,
+				form.email.data,
+				form.password.data,
+				)
+			db.session.add(new_user)
+			db.session.commit()
+			flash('Thanks for registering. Please login.')
+			return redirect(url_for('login'))
+	return render_template('register.html', form=form, error=error)
